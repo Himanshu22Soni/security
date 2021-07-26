1b1c26ff9a91e500a4568928f458b846959ebe53
Himanshu Soni <sonihimanshu2210@gmail.com>
2021-07-26 19:12:15 +0530

    Used Hashing to protect db

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 app.use(express.static("public"));
@@ -20,9 +21,7 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-console.log(process.env.SECRET);
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,7 +38,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password)
   });
 
   newUser.save(function (err) {
@@ -54,7 +53,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const userName = req.body.username;
-  const passWord = req.body.password;
+  const passWord = md5(req.body.password);
 
   User.findOne({ email: userName }, function (err, foundUser) {
     if (foundUser) {
